0114ff15e2fbefe554dd3603fc4fbe37c79611e2
macarena baltra <macarena.bc2011@gmail.com>
2017-05-20 07:05:30 -0400

    Subiendo Ejercicio Terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+// no funciona -.-
+/*
+var caja = document.getElementById('ositos4')[0]; // [0] para que nos recorra desde la posición 0. ositos4 es el id del div donde colocaremos las fotos, en html.
+
+pictures.forEach(fotos){
+  var fotitos = document.createElement('img'); // se crea la etiqueta imagen para colocar las fotos.
+  fotitos.setAttribute('src', fotos); // se colocar src para dar la ubicacion de las imagenes y colocarlas.
+  fotitos.setAttribute('id', ositos4); // se coloca un id para poder despues trabajarla desde css o incluso por js. Con su identificador ositos4.
+
+  caja.appendChild(fotos); // lo coloco en la caja que se creo en html.
+});
+*/
